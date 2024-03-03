913f4ecdff4785adac6c7d613e0bda86061fe272
Santiago del Castillo <sjdelcastilloc@gmail.com>
2024-03-03 19:01:02 -0400

    second commit

--- src/hotebookingsystem/Client.java
+++ src/hotebookingsystem/Client.java
@@ -9,5 +9,29 @@ package hotebookingsystem;
  * @author Santiago
  */
 public class Client {
+    String name;
+    String ID;
+
+    public Client(String name, String ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
     
 }
