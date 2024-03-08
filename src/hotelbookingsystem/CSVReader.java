53ebe66307709ed7d45d4cc0de30192396475215
santidelcastillo03 <sjdelcastilloc@gmail.com>
2024-03-08 13:44:57 -0400

    new hotel, reservation, CSVReader, room classes

--- src/hotelbookingsystem/CSVReader.java
+++ src/hotelbookingsystem/CSVReader.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package hotebookingsystem;
+package hotelbookingsystem;
 
 /**
  *
- * @author Santiago
+ * @author santiagodelcastillo
  */
-public class Booking {
+public class CSVReader {
     
 }
