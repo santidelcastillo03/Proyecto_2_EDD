cedc11c6654f8686d23bf44c460ba8355dc6de06
aanggp <angelggpd@gmail.com>
2024-03-13 21:12:11 -0400

    BST ND BST NODE

--- src/DataStructures/BST.java
+++ src/DataStructures/BST.java
@@ -6,8 +6,37 @@ package DataStructures;
 
 /**
  *
- * @author Santiago
+ * @author Angel
  */
-public class BST<T extends Comparable<T>> {
-    
+public class BST {
+    BSTNode root;
+    public BST(){
+        root = null;
+    }
+    //INSERT NODE (METHOD)
+    public void addNode(int d, String n){
+        BSTNode newn = new BSTNode(d,n);
+        if(root==null){
+            root=newn;
+        }else{
+            BSTNode auxiliar=root;
+            BSTNode father;
+            while(true){
+                father=auxiliar;
+                if(d<auxiliar.data){
+                    auxiliar=auxiliar.leftSon;
+                    if(auxiliar==null){
+                        father.leftSon = newn;
+                        return;
+                    }
+                }else{
+                    auxiliar=auxiliar.rightSon;
+                    if(auxiliar==null){
+                        father.rightSon= newn;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
--- src/DataStructures/BSTNode.java
+++ src/DataStructures/BSTNode.java
@@ -6,8 +6,20 @@ package DataStructures;
 
 /**
  *
- * @author Santiago
+ * @author Angel
  */
 public class BSTNode {
-    
+    int data;
+    String name;
+    BSTNode leftSon, rightSon;
+    public BSTNode(int d, String n){
+        this.data = d;
+        this.name = n;
+        this.leftSon = null;
+        this.rightSon = null;
+    }
+    public String toString(){
+        return name + "sus datos son: " + data;
+    }
+
 }
